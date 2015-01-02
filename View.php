ccb533e6966b03b88cb3cdba6c4706ea3796216b
Komly <komly@yandex.ru>
2015-01-03 02:11:30 +0300

    Initial commit

---
+++ View.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+    Simple template engine for PHP
+*/
+class View {
+    
+    protected $template;
+
+    protected $data;
+
+    protected $multilang;
+
+    public function __construct($template, $data = array(), Multilang $multilang = null)
+    {
+            $this->template  = $template;
+            $this->data      = $data;
+            $this->multilang = $multilang;
+    }
+    
+    public function setData($data) {
+        $this->data = $data;
+    }
+
+    public function setMultilang($multilang) {
+        $this->multilang = $multilang;
+    }
+
+    public function render()
+    {
+        $data = $this->data;
+        $template = $this->template;
+        if ($this->multilang) {
+            $template = preg_replace_callback('/%\s*(\w+)\s*%/', function($matches) use ($multilang) {
+                return $multilang->translate($match[1]);
+            });
+        }
+
+        $result = preg_replace_callback('/{{\s*(\w+)\s*}}/', function($matches) use ($data) {
+            return $data[$matches[1]];
+        }, $template);
+
+        return $result;
+    }
+}
